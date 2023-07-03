74a948c75c2125a98473f3caa110574eb71dc3d6
DanDaMan23 <danlawrcons.gmail.com>
2023-07-03 16:43:04 -0500

    Added show and hide backdrop functionality

--- src/features/navigation-bar/mobile/navigation-bar.mobile.tsx
+++ src/features/navigation-bar/mobile/navigation-bar.mobile.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react"
 import Backdrop from "../../../components/backdrop/backdrop.component"
 import HamburgerButton from "../../../components/hamburger-button/hamburger-button.component"
 import "./navigation-bar.mobile.scss"
 
 export default function NavigationBar() {
+  const [showMobileNavigation, setShowMobileNavigation] = useState(false)
+
   return (
     <>
       <div className='navigation-bar'>
         <HamburgerButton
           onClick={() => {
-            console.log("Hello World")
+            setShowMobileNavigation(true)
           }}
         />
         <h2>DanDaMan</h2>
       </div>
-      <Backdrop show={false} onClick={() => {}} />
+      <Backdrop
+        show={showMobileNavigation}
+        onClick={() => setShowMobileNavigation(false)}
+      />
     </>
   )
 }
