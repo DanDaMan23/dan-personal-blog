82bb0381e040ac0057a3c8cdf9f38500c7295d94
DanDaMan23 <danlawrcons.gmail.com>
2023-07-03 17:01:14 -0500

    Added navigation drawer

---
+++ src/features/navigation-bar/mobile/drawer/navigation-drawer.mobile.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react"
+
+import "./navigation-drawer.mobile.scss"
+
+interface NavigationDrawerProps {
+  show: boolean
+  onClose: () => void
+  children: ReactNode
+}
+
+export default function NavigationDrawer({
+  show,
+  onClose,
+  children
+}: NavigationDrawerProps) {
+  return (
+    <div
+      style={{ display: show ? "block" : "none" }}
+      className='navigation-drawer'
+    >
+      <button type='button' onClick={onClose}>
+        Close
+      </button>
+      <div>{children}</div>
+    </div>
+  )
+}
--- src/features/navigation-bar/mobile/navigation-bar.mobile.tsx
+++ src/features/navigation-bar/mobile/navigation-bar.mobile.tsx
@@ -2,24 +2,30 @@ import { useState } from "react"
 import Backdrop from "../../../components/backdrop/backdrop.component"
 import HamburgerButton from "../../../components/hamburger-button/hamburger-button.component"
 import "./navigation-bar.mobile.scss"
+import NavigationDrawer from "./drawer/navigation-drawer.mobile"
 
 export default function NavigationBar() {
   const [showMobileNavigation, setShowMobileNavigation] = useState(false)
 
+  const openNavigationDrawer = () => {
+    setShowMobileNavigation(true)
+  }
+
+  const closeNavigationDrawer = () => setShowMobileNavigation(false)
+
   return (
     <>
       <div className='navigation-bar'>
-        <HamburgerButton
-          onClick={() => {
-            setShowMobileNavigation(true)
-          }}
-        />
+        <HamburgerButton onClick={openNavigationDrawer} />
         <h2>DanDaMan</h2>
       </div>
-      <Backdrop
+      <Backdrop show={showMobileNavigation} onClick={closeNavigationDrawer} />
+      <NavigationDrawer
         show={showMobileNavigation}
-        onClick={() => setShowMobileNavigation(false)}
-      />
+        onClose={closeNavigationDrawer}
+      >
+        Home About Me Web Development Books Fitness Boxing Travel Contact Me
+      </NavigationDrawer>
     </>
   )
 }
