8cc34b627e3c16aa55cfed4fb8c019962d9b6752
DanDaMan23 <danlawrcons.gmail.com>
2023-07-10 12:19:18 -0500

    Put logo and changed light color to be purpleish

--- src/features/navigation-bar/mobile/navigation-bar.mobile.tsx
+++ src/features/navigation-bar/mobile/navigation-bar.mobile.tsx
@@ -5,16 +5,20 @@ import "./navigation-bar.mobile.scss"
 import NavigationDrawer from "./drawer/navigation-drawer.mobile"
 import NavItems from "./nav-items/nav-items.mobile"
 import { NavigationMobileContext } from "./context/navigation.mobile.context"
+import logo from '../../../photos/dlawcons_logo.png'
 
 export default function NavigationBarMobile() {
   const { showMobileNavigation, openNavigationDrawer, closeNavigationDrawer } =
     useContext(NavigationMobileContext)
 
+  const LOGO_SIZE = 70
+
   return (
     <>
       <div className='navigation-bar-mobile'>
         <HamburgerButton onClick={openNavigationDrawer} />
-        <h2>Dan</h2>
+        {/* <h2>Dan</h2> */}
+        <img src={logo} alt='DLawCons' width={LOGO_SIZE} height={LOGO_SIZE} />
       </div>
       <Backdrop show={showMobileNavigation} onClick={closeNavigationDrawer} />
       <NavigationDrawer
