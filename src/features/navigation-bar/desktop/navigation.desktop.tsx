f77b4d8e8c0ccd1a060b04c708eb86109a05f176
DanDaMan23 <danlawrcons.gmail.com>
2023-07-13 12:44:06 -0500

    Basics of desktop navigation buttons

--- src/features/navigation-bar/desktop/navigation.desktop.tsx
+++ src/features/navigation-bar/desktop/navigation.desktop.tsx
@@ -1,3 +1,30 @@
+import logo from "../../../photos/dlawcons_logo.png"
+import useNavigationItems from "../use-navigation-items"
+
+import "./navigation.desktop.scss"
+
 export default function NavigationBarDesktop() {
-  return <></>
+  const LOGO_SIZE = 70
+
+  const { navigationItems } = useNavigationItems()
+
+  return (
+    <div className='navigation-bar-desktop'>
+      {/* logo */}
+      <img src={logo} alt='DLawCons' width={LOGO_SIZE} height={LOGO_SIZE} />
+
+      {/* Navigation Buttons */}
+      <nav className='desktop-navigation'>
+        <ul className='desktop-navigation__items'>
+          {navigationItems.map(({ label, onClick }) => (
+            <li className='desktop-navigation__item'>
+              <button type='button' onClick={onClick}>
+                {label}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  )
 }
