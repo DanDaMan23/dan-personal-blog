7547120c957beeda58f7f1521be1adf80aa3c5e7
DanDaMan23 <danlawrcons.gmail.com>
2023-07-15 15:42:43 -0500

    Remove carousel in sub pages for about me

--- src/features/about-me/about-me.tsx
+++ src/features/about-me/about-me.tsx
@@ -1,12 +1,12 @@
-import { Outlet, useNavigate } from "react-router"
+import { Outlet, useLocation, useNavigate } from "react-router"
 import Carousel from "../../components/carousel/carousel.compoent"
 
 import "./about-me.scss"
-import { useRef } from "react"
 
 export default function AboutMe() {
   const navigate = useNavigate()
-  const outletRef = useRef<HTMLDivElement>(null)
+
+  const { pathname } = useLocation()
 
   const carouselItems = [
     {
@@ -14,40 +14,28 @@ export default function AboutMe() {
       title: "Web Development",
       details:
         "Front-end development is my specialty, and I thrive on constantly considering the user experience to enhance their day-to-day productivity.",
-      onClick: () => {
-        navigate("/aboutMe/webDevelopment")
-        outletRef.current?.scrollIntoView({ behavior: "smooth" })
-      }
+      onClick: () => navigate("/aboutMe/webDevelopment")
     },
     {
       image: `url(${require("./photos/Books.jpg")})`,
       title: "Books",
       details:
         "During the challenging pandemic era marked by lockdowns, I made a conscious decision to let go of one unproductive hobby and embraced reading, which has had a profound impact on my life and significantly expanded my knowledge base. ",
-      onClick: () => {
-        navigate("/aboutMe/books")
-        outletRef.current?.scrollIntoView({ behavior: "smooth" })
-      }
+      onClick: () => navigate("/aboutMe/books")
     },
     {
       image: `url(${require("./photos/Boxing.jpg")})`,
       title: "Boxing",
       details:
         "Boxing is not just a sport for physical fitness but also a powerful tool for maintaining mental well-being. Whenever I find myself grappling with frustrations, I always channel them into my punches, finding solace and release in the rhythmic rhythm of the bag.",
-      onClick: () => {
-        navigate("/aboutMe/boxing")
-        outletRef.current?.scrollIntoView({ behavior: "smooth" })
-      }
+      onClick: () => navigate("/aboutMe/boxing")
     },
     {
       image: `url(${require("./photos/Travel.jpg")})`,
       title: "Travel",
       details:
         "Last year, I embarked on a solo journey to Montreal, and the experience proved to be truly amazing. It not only allowed me to discover new places and cultures but also provided a valuable opportunity for self-reflection and personal growth. Through exploring the city independently, I gained a deeper understanding of myself and forged lasting memories that continue to resonate with me.",
-      onClick: () => {
-        navigate("/aboutMe/travel")
-        outletRef.current?.scrollIntoView({ behavior: "smooth" })
-      }
+      onClick: () => navigate("/aboutMe/travel")
     }
   ]
 
@@ -61,10 +49,16 @@ export default function AboutMe() {
 
   return (
     <main className='about-me'>
-      <h1>About Me</h1>
-      <Carousel items={carouselItems} />
+      <button
+        type='button'
+        className='about-me-title'
+        onClick={() => navigate("/aboutMe")}
+      >
+        <h1>About Me</h1>
+      </button>
+      {pathname === "/aboutMe" && <Carousel items={carouselItems} />}
 
-      <div className='outlet' ref={outletRef}>
+      <div className='outlet'>
         <Outlet />
       </div>
     </main>
