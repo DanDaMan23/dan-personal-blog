8f2c302ccd4b230e084d64b8be3462f43224e5de
DanDaMan23 <danlawrcons.gmail.com>
2023-07-16 13:41:00 -0500

    Created Web Development Page

---
+++ src/features/about-me/developer_journey/developer-journey.tsx
@@ -0,0 +1,34 @@
+import text from "./developer-journey.json"
+
+import "./developer-journey.scss"
+
+// Not sure if I just wanted Web development so I just name the function DeveloperJourney
+export default function DeveloperJourney() {
+  return (
+    <div className='developer-journey'>
+      <h3>{text.webDevelopment.title}</h3>
+      <div className='developer-journey-image__container'>
+        <img
+          className='developer-journey-image'
+          src={require("./photos/Web_Development_Setup.jpg")}
+          alt='Web Development Setup'
+          width='100%'
+        />
+      </div>
+      <h4>{text.webDevelopment.specializationDetails.heading}</h4>
+      <p>{text.webDevelopment.specializationDetails.details}</p>
+
+      <h4>{text.webDevelopment.learningDetails.heading}</h4>
+      <p>{text.webDevelopment.learningDetails.details}</p>
+
+      <h4>{text.webDevelopment.learningMethods.heading}</h4>
+      <p>{text.webDevelopment.learningMethods.details[0]}</p>
+      <p>{text.webDevelopment.learningMethods.details[1]}</p>
+
+
+      <h5>{text.webDevelopment.learningMethods.analogy.heading}</h5>
+      <p>{text.webDevelopment.learningMethods.analogy.details[0]}</p>
+      <p>{text.webDevelopment.learningMethods.analogy.details[1]}</p>
+    </div>
+  )
+}
