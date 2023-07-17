6537fb41c514be8e148c66224ffdcfe6ef2ae7ec
DanDaMan23 <danlawrcons.gmail.com>
2023-07-16 19:37:54 -0500

    Top books section created

--- src/features/about-me/reading-hobby/reading-hobby.tsx
+++ src/features/about-me/reading-hobby/reading-hobby.tsx
@@ -1,4 +1,8 @@
+import TopBookSection from "./top-book-section/top-book-section.component"
+import extremeOwnershipPhoto from './photos/ExtremeOwnership.jpg'
+import definingDecadePhoto from './photos/TheDefiningDecade.jpg'
 import text from "./reading-hobby.json"
+
 
 import "./reading-hobby.scss"
 
@@ -10,6 +14,18 @@ export default function ReadingHobby() {
       <p>{text.mainDescription}</p>
 
       <h4>{text.topBooks.title}</h4>
+      <TopBookSection
+        title={text.topBooks.extremeOwnership.title}
+        author={text.topBooks.extremeOwnership.author}
+        details={text.topBooks.extremeOwnership.lessonLearned}
+        imageLocation={extremeOwnershipPhoto}
+      />
+      <TopBookSection
+        title={text.topBooks.definingDecade.title}
+        author={text.topBooks.definingDecade.author}
+        details={text.topBooks.definingDecade.lessonLearned}
+        imageLocation={definingDecadePhoto}
+      />
     </div>
   )
 }
