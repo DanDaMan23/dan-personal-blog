6d0a509102c90559d6e8dea096da0174c1f2ab35
DanDaMan23 <danlawrcons.gmail.com>
2023-07-16 20:02:34 -0500

    Created other lists of books I read

--- src/features/about-me/reading-hobby/reading-hobby.tsx
+++ src/features/about-me/reading-hobby/reading-hobby.tsx
@@ -1,8 +1,7 @@
 import TopBookSection from "./top-book-section/top-book-section.component"
-import extremeOwnershipPhoto from './photos/ExtremeOwnership.jpg'
-import definingDecadePhoto from './photos/TheDefiningDecade.jpg'
+import extremeOwnershipPhoto from "./photos/ExtremeOwnership.jpg"
+import definingDecadePhoto from "./photos/TheDefiningDecade.jpg"
 import text from "./reading-hobby.json"
-
 
 import "./reading-hobby.scss"
 
@@ -10,7 +9,6 @@ export default function ReadingHobby() {
   return (
     <div className='reading-hobby'>
       <h3>{text.mainTitle}</h3>
-
       <p>{text.mainDescription}</p>
 
       <h4>{text.topBooks.title}</h4>
@@ -26,6 +24,25 @@ export default function ReadingHobby() {
         details={text.topBooks.definingDecade.lessonLearned}
         imageLocation={definingDecadePhoto}
       />
+
+      <h3>{text.otherBooks.mainTitle}</h3>
+      <div className='other-books-section'>
+        <h4>{text.otherBooks.fictions.title}</h4>
+        <ul>
+          {text.otherBooks.fictions.books.map((book) => (
+            <li>{book}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className='other-books-section'>
+        <h4>{text.otherBooks.nonFictions.title}</h4>
+        <ul>
+          {text.otherBooks.nonFictions.books.map((book) => (
+            <li>{book}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
