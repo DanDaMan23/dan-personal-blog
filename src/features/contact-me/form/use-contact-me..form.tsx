9e023d6d14a025d74183df63deb755800d6d08c6
DanDaMan23 <danlawrcons.gmail.com>
2023-07-19 15:06:28 -0500

    Form logic redesigned using react hook form package

--- src/features/contact-me/form/use-contact-me..form.tsx
+++ src/features/contact-me/form/use-contact-me..form.tsx
@@ -1,120 +1,63 @@
-import { ChangeEvent, FormEvent, useState } from "react"
+import { FormEvent } from "react"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 import emailjs from "@emailjs/browser"
 
 export default function useContactMeForm() {
-  interface IContactMeFormValues {
-    fullName: string
-    email: string
-    subject: string
-    message: string
-  }
+  const contactMeFormSchema = yup
+    .object({
+      fullName: yup.string().required(),
+      email: yup.string().required(),
+      subject: yup.string().required(),
+      message: yup.string().required()
+    })
+    .required()
 
-  const initialFormValues: IContactMeFormValues = {
-    fullName: "",
-    email: "",
-    subject: "",
-    message: ""
-  }
+  type ContactMeFormData = yup.InferType<typeof contactMeFormSchema>
 
-  const [contactMeFormValues, setContactMeFormValues] =
-    useState<IContactMeFormValues>(initialFormValues)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitSuccessful },
+    reset
+  } = useForm<ContactMeFormData>({
+    resolver: yupResolver(contactMeFormSchema)
+  })
 
-  const [validationErrors, setValidationErrors] = useState<
-    Partial<IContactMeFormValues>
-  >({})
-
-  const [formSubmissionStatus, setFormSubmissionStatus] = useState<
-    "init" | "loading" | "done"
-  >("init")
-
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target
-
-    setContactMeFormValues((prev) => ({ ...prev, [name]: value }))
-  }
-
-  const handleValidation = () => {
-    setValidationErrors({})
-
-    if (contactMeFormValues.fullName.trim().length === 0) {
-      setValidationErrors((prev) => ({
-        ...prev,
-        fullName: "Full Name is Required"
-      }))
-    }
-
-    if (contactMeFormValues.email.trim().length === 0) {
-      setValidationErrors((prev) => ({
-        ...prev,
-        email: "Email is Required"
-      }))
-    }
-
-    if (contactMeFormValues.subject.trim().length === 0) {
-      setValidationErrors((prev) => ({
-        ...prev,
-        subject: "Subject is Required"
-      }))
-    }
-
-    if (contactMeFormValues.message.trim().length === 0) {
-      setValidationErrors((prev) => ({
-        ...prev,
-        message: "Message is Required"
-      }))
-    }
-  }
-
-  const handleSubmit = async (e: FormEvent) => {
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault()
-
-    handleValidation()
-
-    // If error occur, then stop the process, otherwise continue
-    if (
-      Object.keys(validationErrors).length !== 0 &&
-      Object.values(contactMeFormValues).every(
-        (value) => value.trim().length !== 0
-      )
-    ) {
-      setFormSubmissionStatus("loading")
+    handleSubmit(async (data: ContactMeFormData) => {
       await emailjs
         .send(
           "service_tfdxghl",
           "template_cb5h6mg",
           {
-            ...contactMeFormValues
+            ...data
           },
           "B1TgxrcFscxkVz81G"
         )
         .then((response) => {
           console.log(response)
+          reset()
         })
         .catch((error) => {
           console.log(error)
         })
-
-      setContactMeFormValues(initialFormValues)
-      setFormSubmissionStatus("done")
-    }
+    })()
   }
 
-  const handleClear = (e: FormEvent) => {
+  const onClear = (e: FormEvent) => {
     e.preventDefault()
-
-    setContactMeFormValues(initialFormValues)
-    setValidationErrors({})
+    reset()
   }
 
   return {
-    handleChange,
-    handleSubmit,
-    contactMeFormValues,
-    handleClear,
-    validationErrors,
-    handleValidation,
-    formSubmissionStatus
+    register,
+    onSubmit,
+    onClear,
+    errors,
+    isValid,
+    isSubmitSuccessful
   }
 }
