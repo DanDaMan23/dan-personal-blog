2a7819aea4e8819cbdd4915650d2cf40452bf4e7
DanDaMan23 <danlawrcons.gmail.com>
2023-07-19 16:26:06 -0500

    Added logo to drawer

--- src/features/navigation-bar/mobile/drawer/navigation-drawer.mobile.tsx
+++ src/features/navigation-bar/mobile/drawer/navigation-drawer.mobile.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faClose } from "@fortawesome/free-solid-svg-icons"
+import logo from '../../../../photos/dlawcons_logo.png'
 
 import "./navigation-drawer.mobile.scss"
 
@@ -21,6 +22,9 @@ export default function NavigationDrawer({
         <FontAwesomeIcon icon={faClose} />
       </button>
       <div>{children}</div>
+      <div className="logo-area">
+        <img src={logo} alt='DLawCons' height={100} />
+      </div>
     </div>
   )
 }
