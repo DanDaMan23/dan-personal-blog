d56ec8af44643553513fcb5ebd0744fd8e1c7c53
DanDaMan23 <danlawrcons.gmail.com>
2023-07-20 15:06:58 -0500

    Created montreal trip section

---
+++ src/features/about-me/travel/montreal-trip/montreal-trip.tsx
@@ -0,0 +1,29 @@
+import text from "../travel.json"
+
+interface MontrealTripProps {
+  show: boolean
+}
+
+export default function MontrealTrip({ show }: MontrealTripProps) {
+  const montrealTripImages = require.context("./photos", true)
+  const montrealTripImageList = montrealTripImages
+    .keys()
+    .map((image) => montrealTripImages(image))
+
+  return (
+    <div
+      className='montreal-trip'
+      style={{
+        display: show ? "block" : "none"
+      }}
+    >
+      <h4>{text.trips.montreal.title}</h4>
+      <p>{text.trips.montreal.description}</p>
+      <div className='photos-section'>
+        {montrealTripImageList.map((image, index) => (
+          <img key={index} src={image} alt={image} />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/features/about-me/travel/travel.tsx
+++ src/features/about-me/travel/travel.tsx
@@ -1,11 +1,12 @@
 import TabSwitcher from "../../../components/tab-switcher/tab-switcher.component"
 import useTabSwitcher from "../../../components/tab-switcher/use-tab-switcher.component"
+import MontrealTrip from "./montreal-trip/montreal-trip"
 import text from "./travel.json"
 
 import "./travel.scss"
 
 export default function Travel() {
-  const tabs = ["Montreal 2022", "New York September 2023"]
+  const tabs = [text.trips.montreal.title, "New York September 2023 Plan"]
 
   const tabSwitcherProps = useTabSwitcher(tabs)
 
@@ -16,6 +17,18 @@ export default function Travel() {
       <p>{text.mainDescription}</p>
 
       <TabSwitcher {...tabSwitcherProps} />
+
+      <div className='trip-section'>
+        <MontrealTrip show={tabSwitcherProps.activeTab === tabs[0]} />
+        <div
+          className='new-york-trip'
+          style={{
+            display: tabSwitcherProps.activeTab === tabs[1] ? "block" : "none"
+          }}
+        >
+          {tabSwitcherProps.activeTab}
+        </div>
+      </div>
     </div>
   )
 }
