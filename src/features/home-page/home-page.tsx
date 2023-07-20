a2801bbf7887979c10f8786d2435c0747afb6f50
DanDaMan23 <danlawrcons.gmail.com>
2023-07-20 16:10:29 -0500

    Added photoin my homepage

--- src/features/home-page/home-page.tsx
+++ src/features/home-page/home-page.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router"
+import mainPicture from "../../photos/MainBlogPicture.jpg"
 import "./home-page.scss"
 
 export default function HomePage() {
@@ -6,22 +7,27 @@ export default function HomePage() {
 
   return (
     <main className='home-page'>
-      <div>
-        <h2>Welcome to My World, I am Dan Lawrence Consengco</h2>
-        <p>
-          I am a Front-End Software Developer passionate about
-          JavaScript/TypeScript, constantly learning in the ever-evolving tech
-          world. Practice and repetition unlock the gates to continuous growth.
-          I am deeply committed to enhancing user experiences, always striving
-          to deliver the absolute best.
-        </p>
-        <button
-          type='button'
-          className='primary-button'
-          onClick={() => navigate("/aboutMe")}
-        >
-          Learn More About Me...
-        </button>
+      <div className='content-container'>
+        <div className="image-container">
+          <img src={mainPicture} alt='DLawCons' />
+        </div>
+        <div>
+          <h2>Welcome to My World, I am Dan Lawrence Consengco</h2>
+          <p>
+            I am a Front-End Software Developer passionate about
+            JavaScript/TypeScript, constantly learning in the ever-evolving tech
+            world. Practice and repetition unlock the gates to continuous
+            growth. I am deeply committed to enhancing user experiences, always
+            striving to deliver the absolute best.
+          </p>
+          <button
+            type='button'
+            className='primary-button'
+            onClick={() => navigate("/aboutMe")}
+          >
+            Learn More About Me...
+          </button>
+        </div>
       </div>
     </main>
   )
