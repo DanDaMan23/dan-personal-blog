89702dc255883da6a7bb51f1a20cc8039f2d7910
DanDaMan23 <danlawrcons.gmail.com>
2023-07-21 14:08:21 -0500

    Can navigate by clicking the logo in nav bar

--- src/features/navigation-bar/desktop/navigation.desktop.tsx
+++ src/features/navigation-bar/desktop/navigation.desktop.tsx
@@ -4,7 +4,7 @@ import useNavigationItems from "../use-navigation-items"
 import Accordion from "../../../components/accordion/accordion.component"
 
 import "./navigation.desktop.scss"
-import { useLocation } from "react-router"
+import { useLocation, useNavigate } from "react-router"
 
 export default function NavigationBarDesktop() {
   const LOGO_SIZE = 70
@@ -13,9 +13,17 @@ export default function NavigationBarDesktop() {
 
   const { pathname } = useLocation()
 
+  const navigate = useNavigate()
+
   return (
     <div className='navigation-bar-desktop'>
-      <img src={logo} alt='DLawCons' height={LOGO_SIZE} />
+      <button
+        type='button'
+        className='logo-button'
+        onClick={() => navigate("/")}
+      >
+        <img src={logo} alt='DLawCons' height={LOGO_SIZE} />
+      </button>
 
       <nav className='desktop-navigation'>
         <ul className='desktop-navigation__items'>
