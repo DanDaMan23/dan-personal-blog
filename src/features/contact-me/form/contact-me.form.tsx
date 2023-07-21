03b767c6722c5eb4af294a524ceaaaf104576732
DanDaMan23 <danlawrcons.gmail.com>
2023-07-21 13:31:32 -0500

    Changed font family

--- src/features/contact-me/form/contact-me.form.tsx
+++ src/features/contact-me/form/contact-me.form.tsx
@@ -12,28 +12,33 @@ export default function ContactMeForm() {
     <form onSubmit={onSubmit} className='contact-me-form'>
       <div className='form-field'>
         <label htmlFor='fullName'>Full Name: </label>
-        <input {...register("fullName")} />
+        <input {...register("fullName")} placeholder='Enter your Full Name' />
         {errors.fullName && (
           <ErrorMessage className='error-field' message='Fullname Required' />
         )}
       </div>
       <div className='form-field'>
         <label htmlFor='email'>Email: </label>
-        <input {...register("email")} />
+        <input {...register("email")} placeholder='Enter your Email' />
         {errors.email && (
           <ErrorMessage className='error-field' message='Email Required' />
         )}
       </div>
       <div className='form-field'>
         <label htmlFor='subject'>Subject: </label>
-        <input {...register("subject")} />
+        <input {...register("subject")} placeholder='Enter the Subject' />
         {errors.subject && (
           <ErrorMessage className='error-field' message='Subject Required' />
         )}
       </div>
       <div className='form-field'>
         <label htmlFor='message'>Message: </label>
-        <textarea {...register("message")} cols={5} rows={10}></textarea>
+        <textarea
+          {...register("message")}
+          cols={5}
+          rows={10}
+          placeholder='Enter your Message'
+        ></textarea>
         {errors.message && (
           <ErrorMessage className='error-field' message='Message Required' />
         )}
