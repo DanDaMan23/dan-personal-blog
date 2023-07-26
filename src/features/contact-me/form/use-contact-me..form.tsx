11811bc3c1e146087381308a0d958a25a98759b0
DanDaMan23 <danlawrcons.gmail.com>
2023-07-26 11:35:55 -0500

    Fix validation messages and some text

--- src/features/contact-me/form/use-contact-me..form.tsx
+++ src/features/contact-me/form/use-contact-me..form.tsx
@@ -3,16 +3,23 @@ import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import emailjs from "@emailjs/browser"
+import text from "./contact-me.form.json"
 
 export default function useContactMeForm() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const contactMeFormSchema = yup
     .object({
-      fullName: yup.string().required(),
-      email: yup.string().required(),
-      subject: yup.string().required(),
-      message: yup.string().required()
+      fullName: yup.string().required(text.fullName.validations.required),
+      email: yup
+        .string()
+        .required(text.email.validations.required)
+        .matches(
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          text.email.validations.invalidEmail
+        ),
+      subject: yup.string().required(text.subject.validations.required),
+      message: yup.string().required(text.message.validations.required)
     })
     .required()
 
