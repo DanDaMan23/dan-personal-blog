87c621a2762f417ada5ec4b2f827bcc987314ac0
DanDaMan23 <danlawrcons.gmail.com>
2023-10-17 21:02:39 -0500

    New York trip description updated

--- src/features/about-me/travel/new-york-trip/new-york-trip.tsx
+++ src/features/about-me/travel/new-york-trip/new-york-trip.tsx
@@ -12,7 +12,6 @@ export default function NewYorkTrip() {
     <div className='new-york-trip'>
       <h4>{text.trips.newYork.title}</h4>
       <p>{text.trips.newYork.description}</p>
-      <h4>{text.trips.newYork.thingsToDo.title}</h4>
       <div className='photos-section'>
         {newYorkTripImageList.map((image, index) => (
           <img key={index} src={image} alt={image} />
